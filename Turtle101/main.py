e6f8c23471d264b30dc3bd50d83bd88a6b1cf6d5
Hilmi Türksoy <hilmiturksoy@gmail.com>
2023-11-26 18:10:04 +0300

    star drawed

---
+++ Turtle101/main.py
@@ -0,0 +1,15 @@
+import turtle
+
+drowing_board=turtle.Screen()
+drowing_board.bgcolor("yellow")
+drowing_board.title("python turtle")
+turtle_instance=turtle.Turtle()
+
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle.done()
---
+++ Turtle101/star.py
@@ -0,0 +1,18 @@
+# import for turtle
+import turtle
+
+# Starting a Working Screen
+star_Turtle = turtle.Screen()
+star_Turtle = turtle.bgcolor("yellow")
+
+# initializing a turtle instance
+star_Turtle = turtle.Turtle()
+
+# executing loop 5 times for a star
+for i in range(5):
+
+		# moving turtle 100 units forward
+		star_Turtle.forward(200)
+
+		# rotating turtle 144 degree right
+		star_Turtle.right(144)
